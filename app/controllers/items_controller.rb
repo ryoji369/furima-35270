4631902ce346a1ba7b5854342cbbb26f173f2b52
ryoji369 <ryoji570510@icloud.com>
2021-05-13 17:18:49 +0900

    商品編集機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
@@ -53,7 +54,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != @item.user_id && @item.buyer.present?
+    if current_user.id != @item.user_id || (current_user.id == @item.user_id && @item.buyer.present?)
       redirect_to root_path
     end
   end
